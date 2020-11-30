e06d284c8979dcbe0c523818aa2cac909e8a332b
3dunoabraham <71507184+3dunoabraham@users.noreply.github.com>
2020-11-29 20:51:46 -0400

    Update text-view.js

--- text-view.js
+++ text-view.js
@@ -1,49 +1,53 @@
-class EXAMPLE {
-	constructor($container, options, data = null)
-	{
-		this.$container = $($container);
+class jQueryTemplateView {
+			constructor($container, options, data = null)
+			{
+				this.$container = $($container);
 
-		this.options = Object.assign({
-		}, options);
+				this.options = Object.assign({
+				}, options);
 
-		this.data = Object.assign({
-		}, data);
+				this.data = Object.assign({
+				}, data);
 
-		let self = this;
+				let self = this;
 
-		this.requests =
-		{
-		};
+				this.requests =
+				{
+				};
 
-		this.updaters =
-		{
-		};
+				this.updaters =
+				};
 
-		this.watchers =
-		{
-		};
+				this.watchers =
+				{
+				};
 
-		this.clickers =
-		{
-		};
+				this.clickers =
+				{
+				};
 
-		this.initWatchers();
-	}
+				this.initWatchers();
+			}
 
-	initWatchers()
-	{
-		let self = this;
+			initWatchers()
+			{
+				let self = this;
 
-		this.$container.on('change',"[data-watch]", function(event)
-		{
-			self.watchers[$(this).data('watch')](event);
+				this.$container.on('change',"[data-watch]", function(event)
+				{
+					self.watchers[$(this).data('watch')](event);
+				});
+
+				this.$container.on('click',"[data-click]", function(event)
+				{
+					self.clickers[$(this).data('click')](event);
+				});
+			}
+		}
+
+		$(document).ready(function(){
+
+			let newjQueryTemplateView = new jQueryTemplateView('#templateViewId', {
+			});
+
 		});
-		this.$container.on('click',"[data-click]", function(event)
-		{
-			self.clickers[$(this).data('click')](event);
-		});
-	}
-}
-
-let newEXAMPLE = new EXAMPLE('#', {
-});
